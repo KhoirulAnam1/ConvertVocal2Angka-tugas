b2f19803056960c921f1ead70deceb86dd281ec2
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-08-15 19:45:53 +0700

    Tambah konversi

--- src/ConverVocal2Angka.java
+++ src/ConverVocal2Angka.java
@@ -31,15 +31,26 @@ public class ConverVocal2Angka {
         tampilPerKata(kalimat, convert);
         tampilHasil(convert);
         
-         private static String tampilInput() {
+        private static String tampilInput() {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Masukkan Kalimat: ");
         String kalimat = scanner.nextLine();
         System.out.println("Kalimat Asli : " + kalimat);
         
-        return kalimat;
-    }    
+        return kalimat;}
+        
+        private static String vocal2Angka(String kalimat) {
+            char [][] arConvert = 
+            {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+            
+            kalimat = kalimat.toLowerCase();
+            for(int i=0;i<arConvert.length;i++)
+                kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+            
+            return kalimat;
+        }
+        
     }
     
 
