d883697035243e5de4991c321def3e30061b8826
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-08-15 19:58:55 +0700

    Initial Commit

--- src/ConverVocal2Angka.java
+++ src/ConverVocal2Angka.java
@@ -30,44 +30,16 @@ public class ConverVocal2Angka {
         String convert = vocal2Angka(kalimat);
         tampilPerKata(kalimat, convert);
         tampilHasil(convert);
+    }
+    
+    
         
-        private static String tampilInput() {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Masukkan Kalimat: ");
-        String kalimat = scanner.nextLine();
-        System.out.println("Kalimat Asli : " + kalimat);
-        
-        return kalimat;}
-        
-        private static String vocal2Angka(String kalimat) {
-            char [][] arConvert = 
-            {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
-            
-            kalimat = kalimat.toLowerCase();
-            for(int i=0;i<arConvert.length;i++)
-                kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
-            
-            return kalimat;
-        }
-        
-         private static void tampilPerKata(String kalimat, String convert) {
-            String[] arrKal = kalimat.split(" ");
-            String[] arrCon = convert.split(" ");
-            
-            for(int i=0;i<arrKal.length;i++)
-                System.out.println(arrKal[i]+" => "+arrCon[i]);
-        }
-         
-          private static void tampilHasil(String convert) {
-            System.out.println("Kalimat Alay Angka : " + convert);
-        }
         
     }
     
 
    
-    }
+    
 
     
     
