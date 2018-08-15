faa20739adfadfaa6e53cbeaf373176cd5372846
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-08-15 19:27:04 +0700

    Coba method compareTo & compareToIgnoreCase

--- src/CobaOperasiString.java
+++ src/CobaOperasiString.java
@@ -42,7 +42,19 @@ public class CobaOperasiString {
                 + x.equals(r));
         System.out.println("isi x sama dengan r (Not Case Sensitive): "
                 + x.equalsIgnoreCase(r));
-
+        
+        System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
+        System.out.println("isi x sama dengan z (Case Sensitive): "
+                + x.compareTo(z));
+        System.out.println("isi x sama dengan r (Case Sensitive): "
+                + x.compareTo(r));
+        System.out.println("isi x sama dengan r (Not Case Sensitive): "
+                + x.compareToIgnoreCase(r));
+        
+        String s = "operasi";
+        System.out.println("isi variabel s : " + s);
+        System.out.println("isi x sama dengan s (Case Sensitive): "
+                + x.compareTo(s));
     }
     
 }
