1eaa2f1ce06dde61d4d0c8d090966bbeabf4121a
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-08-15 20:01:09 +0700

    Tambah Tampil Per Kata

--- src/ConverVocal2Angka.java
+++ src/ConverVocal2Angka.java
@@ -53,6 +53,14 @@ public class ConverVocal2Angka {
             
             return kalimat;
         }
+    
+     private static void tampilPerKata(String kalimat, String convert) {
+            String[] arrKal = kalimat.split(" ");
+            String[] arrCon = convert.split(" ");
+            
+            for(int i=0;i<arrKal.length;i++)
+                System.out.println(arrKal[i]+" => "+arrCon[i]);
+        }
         
         
     }
